055de7ea074fb5ac542872030f9a5c78ce1b7fe6
akramen <michael.yamada.5g@gmail.com>
2021-09-22 20:28:54 +0900

    9/22/20:28

---
+++ index.js
@@ -0,0 +1,21 @@
+const { Client, Intents, MessageActionRow, MessageButton,} = require('discord.js')
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+//If the name of the slash command is 'goup'
+if (interaction.commandName === 'goup'){
+    const row = new MessageActionRow()
+        .addComponents(
+            new MessageButton()
+                .setLabel('Please read it here.')
+                .setStyle('LINK')
+                .setURL("messagelink, etc."), //Please put the URL here.
+        );
+    await interaction.reply({content: "‌ ",components: [row]});
+};
+
+client.login('YOUR_BOT_TOKEN');//Please put the BOTtoken here.
